b427fb708f25cd6a66784283ca6799ae6c6633b3
Daniel <danielflorestamayo@gmail.com>
2019-05-14 17:45:48 -0500

    prueba

---
+++ Prueba1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <h1 //align = "center">TiendaEC</h1>
+	</head>
+	<body>
+<form method="post">
+     <center>
+     <h2 //align = "center">Registro de tienda</h2>
+     <div>
+     <label>Nombre de tienda : <input type="text" id="usrid" name="tienda" required ="required"  /></label>
+     </div>
+     <br>
+     <div>
+     <label>Usuario : <input type="text" id="usrid" name="user" required ="required"  /></label>
+     </div>
+     <br>
+     <div>
+     <label>Clave : <input type ="password" name = "pass"  required ="required"></label>
+     </div>
+     <br>
+     <div>
+     <label>Repetir clave : <input type ="password" name = "pass"  required ="required"></label>
+     </div>
+     </center>
+      <br>
+      <center><input type="submit" value="REGISTRAR"/> </center>
+</form>
+   
+
+	</body>
+
+</html>	
+
+
+
+
+
+
